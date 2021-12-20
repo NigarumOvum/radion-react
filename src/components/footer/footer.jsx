f12110a8a6a29e20fa967320b2126e5122cc9b56
Brealy Fabian Padron Rodriguez <48064359+NigarumOvum@users.noreply.github.com>
2021-12-20 16:31:28 -0600

    Update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -49,7 +49,7 @@ function Footer() {
                     <img src={PLogo} alt='Logo' />
                     <p>
                         <Gradient dir="left-to-right" from="#00DFD8" to="#007CF0">
-                            Designed & Developed by <a href="https://www.neighbordevcr.com">NeighborDev CR @Copyright</a>{new Date().getFullYear()}
+                            Designed & Developed by <a href="https://neighbordevcr.com">NeighborDev CR @Copyright</a>{new Date().getFullYear()}
                         </Gradient>
                     </p>
                 </div>
